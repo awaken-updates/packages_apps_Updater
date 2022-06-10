ArrowOS/android_packages_apps_Updater

Utils: Pass our ziptype prop as parameter to server url
- This is to handle and serve responses separately for vanilla and
  gapps builds.

Change-Id: I553f279a821a8be2c8879f92e83d72157287e923

src/org/lineageos/updater/misc/Constants.java
@@ -49,6 +49,7 @@ public final class Constants {
     public static final String PROP_DEVICE = "ro.arrow.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_RELEASE_TYPE = "ro.arrow.releasetype";
+    public static final String PROP_ZIP_TYPE = "ro.arrow.ziptype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "arrow.updater.uri";
 

awaken-updates/packages_apps_Updater

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

src/com/awaken/updater/misc/Constants.java
@@ -46,6 +46,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.awaken.buildtype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "awaken.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "awaken.updater.uri";
+    public static final String PROP_ZIP_TYPE = "ro.awaken.ziptype";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";